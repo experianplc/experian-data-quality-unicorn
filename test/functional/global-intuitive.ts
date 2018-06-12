97f35f0c0097f0e448bd820c19bbc334be0a2fc8
tabeth <tabeth@tabeth.com>
2018-06-11 21:30:33 -0400

    Writing tests for Global Intuitive

--- test/functional/global-intuitive.ts
+++ test/functional/global-intuitive.ts
@@ -4,26 +4,63 @@ const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
 
 
+/* Pages unique for Global Intuitive Tests */
+function fillInAddressField(text: string) {
+  return function() {
+    return this.parent
+      .findByCssSelector('#address-line-one')
+      .type(text)
+      .end()
+  };
+}
+
+function showSuggestions() {
+  return function() {
+    return this.parent
+      .findByCssSelector('#address-line-one')
+      .type(' ')
+  };
+}
+
+function addressPopulated() {
+  return function() {
+    return this.parent
+      .findByCssSelector('#city')
+      .getAttribute('value')
+      .then((val) => {
+        console.log(val);
+        assert.equal(true, Boolean(val), 'City value populated. Global Intuitive functional');
+      })
+  };
+}
+
 registerSuite('Experian Unicorn - Global Intuitive Tests', {
   beforeEach: function() {
     return this.remote
       .setFindTimeout(10000)
-      .get('../global-intuitive-page/index.html')
-      .sleep(20000)
+      .get('localhost:8000/global-intuitive-index.html')
   },
 
   tests: {
     'Global Intuitive suggestions can be shown': function () {
       return this.remote
-        .findByCssSelector('body')
+        .then(fillInAddressField('53 State Street'))
+        .findByCssSelector('.edq-global-intuitive-address-suggestion')
         .getVisibleText()
         .then((text) => {
-          assert.equal(
-            true,
-            false,
-            'Verification redirects to settings when settings are not input'
-          );
+          assert.equal(true, Boolean(text), "Global Intuitive can populate items");
         })
+    }
+
+    'Global Intuitive suggestion can be clicked and populate fields': function() {
+      return this.remote
+        .then(fillInAddressField('53 State Street'))
+        .then(showSuggestions())
+          .findByCssSelector('.edq-global-intuitive-address-suggestion')
+          .click()
+          .sleep(3000) 
+          .end()
+        .then(addressPopulated())
     }
   }
 
